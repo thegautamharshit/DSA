316f0e6b7f087b097b2bef175285f0710fe8af66
Harshit Gautam <thegautamharshit@gmail.com>
2024-03-24 00:27:45 +0530

    Question number 4, Contains Duplicate Solution.

---
+++ DSAsheet/Solution/src/Q4_Contains_Duplicate.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Q4_Contains_Duplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] n ={1,2,3,1};
+        boolean x=containsDuplicate(n);
+        System.out.println(x);
+    }
+}
