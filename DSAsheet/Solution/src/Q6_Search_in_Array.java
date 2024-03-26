bd8a4462d9bcd06901092838fa3035161d8e17e1
Harshit Gautam <thegautamharshit@gmail.com>
2024-03-26 22:51:37 +0530

    Question 6, search in Array

---
+++ DSAsheet/Solution/src/Q6_Search_in_Array.java
@@ -0,0 +1,17 @@
+public class Q6_Search_in_Array {
+
+    static int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target=0;
+        int x=search(nums,target);
+        System.out.println(x);
+    }
+}
